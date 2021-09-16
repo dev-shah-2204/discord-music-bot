c5e2bf41ae740095a960fd91781ed9e993c39abb
dev-shah-2204 <dgm82382@gmail.com>
2021-09-16 15:38:50 +0530

    Set activity and change prefix

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from discord.ext import commands
 class MusicBot(commands.Bot):
     def __init__(self):
         super().__init__(
-            command_prefix='mb ' and 'mb!',
+            command_prefix='m!',
             intentes=discord.Intents.all(),
             case_insensitive=True,
             allowed_mentions=discord.AllowedMentions(everyone=False),
@@ -20,6 +20,5 @@ class MusicBot(commands.Bot):
         print(f"Logged in as {self.user}")
         print(f"ID: {self.user.id}")
 
-
-
-
+        # Set activity to "Listening to m!help"
+        self.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="m!help"))
