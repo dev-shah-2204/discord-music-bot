27c644feb1b12b4fda0e745edbf05014c2f67b2d
dev-shah-2204 <dgm82382@gmail.com>
2021-09-18 15:10:25 +0530

    Initial commit

---
+++ cogs/error_handler.py
@@ -0,0 +1,57 @@
+import discord
+
+from discord.ext import commands
+
+
+class ErrorHandler(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send(f"{ctx.author.mention}, that command is incomplete")
+            ctx.command.reset_cooldown(ctx)
+            return
+
+        elif isinstance(error, commands.BotMissingPermissions):
+            perm = error.missing_permissions[0].title().replace('_', ' ')
+
+            await ctx.send(f"{ctx.author.mention}, I need the `{perm}` permission in order to complete that command")
+            ctx.command.reset_cooldown(ctx)
+            return
+
+        elif isinstance(error, commands.CommandOnCooldown):
+            await ctx.send(f"Hold your horses, that command is still on cooldown ({error.retry_after:,.1f} seconds)")
+            return
+
+        elif isinstance(error, commands.BadArgument):
+            await ctx.send(f"`{error.args}` cannot be the argument for that command. Please run `m!help {ctx.command}`")
+            return
+
+        elif isinstance(error, commands.CommandNotFound):
+            await ctx.send(f"Did you make a typo? Because that command doesn't exist")
+            return
+
+        elif isinstance(error, discord.Forbidden) or isinstance(error, discord.errors.Forbidden):
+            await ctx.send("Uh-oh, an error occurred. An easy fix is to give me the `Embed Links` permission. If that doesn't work, please contact StatTrakDiamondSword#5493")
+
+        else:
+            await ctx.send("An error occured that I wasn't able to handle myself. This has been conveyed to my developer.")
+            channel = self.bot.get_channel(857878860251136020)
+
+            em = discord.Embed(
+                title="Error",
+                description=f"```Command: {ctx.command}\nServer: {ctx.guild} ({ctx.guild.id})\nChannel: {ctx.chanel} ({ctx.channel.id})\nUser: {ctx.author} ({ctx.author.id})```",
+                color=discord.Color.brand_red()
+            )
+            em.add_field(name="Message:", value=ctx.message.content)
+            em.add_field(name="Error", value=f"```{error}```")
+
+            await channel.send(embed=em)
+
+
+
+def setup(bot):
+    bot.add_cog(ErrorHandler(bot))
+    print("ErrorHandler cog loaded")
---
+++ cogs/info.py
@@ -0,0 +1,57 @@
+import discord
+
+from discord.ext import commands
+
+
+class Info(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='ping', help="Show the bot's ping")
+    async def ping(self, ctx):
+        em = discord.Embed(
+            title="Pong!",
+            description=f"{round(self.bot.latency*1000)} ms",
+            color=0x60FF60
+        )
+
+        await ctx.send(embed=em)
+
+
+    @commands.command(name="invite", help="Get the bot's invite link")
+    async def invite(self, ctx):
+        em = discord.Embed(
+            title="Thanks for inviting me!",
+            description=f"Click [here](https://discord.com/api/oauth2/authorize?client_id={self.bot.id}&permissions=446680124480&scope=bot)!",
+            color=0x60FF60
+        )
+
+        await ctx.send(embed=em)
+
+
+    @commands.command(name="stats", help="Get the bot's stats")
+    async def stats(self, ctx):
+        em = discord.Embed(
+            title="Here are my stats!",
+            description="They are very accurate",
+            color=0x60FF60
+        )
+        em.add_field(
+            name="Servers:",
+            value=len(self.bot.guilds)
+        )
+        em.add_field(
+            name="Users",
+            value=len(self.bot.users)
+        )
+        em.add_field(
+            name="Active voice channels",
+            value=len(self.bot.voice_clients)
+        )
+
+        await ctx.send(embed=em)
+
+
+def setup(bot):
+    bot.add_cog(Info(bot))
+    print("Info cog loaded")
